c37c22df4ced8f66ae807baf9683b9f005966323
Elizabeth Lavender <lizernz24@gmail.com>
2024-10-24 16:32:19 +0100

    added the junit5 test and used Mockito to mock the ProductRepository

--- demo/src/main/java/com/example/demo/entity/Item.java
+++ demo/src/main/java/com/example/demo/entity/Item.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
