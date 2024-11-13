4e2b24506c19b914448bee09f47a5af0a9003a68
Elizabeth Lavender <lizernz24@gmail.com>
2024-11-13 19:20:57 +0000

    Altered the security folder for tokens

--- demo/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ demo/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,3 +1,4 @@
+
 package com.example.demo.security;
 
 import jakarta.servlet.FilterChain;
@@ -13,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
 import java.util.ArrayList;
+
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -71,5 +73,3 @@ protected void successfulAuthentication(HttpServletRequest request, HttpServletR
     chain.doFilter(request, response);
 }
 }
-
-   
